df3b8ffb9b23d79f4b44c7aea6c0a44af1b57f88
fabihsuan <fabihsuan@gmail.com>
2021-06-30 17:49:47 +0200

    add newtaipei data

--- new.js
+++ new.js
@@ -1,8 +1,8 @@
-function selectItemByValue(elmnt, value) {
-  for (var i = 0; i < elmnt.options.length; i++) {
-    if (elmnt.options[i].value === value) {
-      elmnt.selectedIndex = i;
-      break;
-    }
-  }
-}
+// function selectItemByValue(elmnt, value) {
+//   for (var i = 0; i < elmnt.options.length; i++) {
+//     if (elmnt.options[i].value === value) {
+//       elmnt.selectedIndex = i;
+//       break;
+//     }
+//   }
+// }
