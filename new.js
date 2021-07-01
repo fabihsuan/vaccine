571d8cdec3e8925c31dd4789d9fc24f3f3333d4f
fabihsuan <fabihsuan@gmail.com>
2021-07-01 16:06:12 +0200

    add top button

--- new.js
+++
@@ -1,8 +0,0 @@
-// function selectItemByValue(elmnt, value) {
-//   for (var i = 0; i < elmnt.options.length; i++) {
-//     if (elmnt.options[i].value === value) {
-//       elmnt.selectedIndex = i;
-//       break;
-//     }
-//   }
-// }
